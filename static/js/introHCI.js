cdc9880455c63c79155db61dfd042cd94bfb6e3e
Ali Buzayan <abuzayan@ucsd.edu>
2021-01-21 10:33:39 -0800

    done through slide 46

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,10 +8,41 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+// function initializePage() {
+// 	$("#testjs").click(function(e) {
+// 		$('.jumbotron h1').text("Javascript is connected");
+// 	});
+
+// 	// Add any additional listeners here
+// 	// example: $("#div-id").click(functionToCall);
+// }
+function projectClick(e) {
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+    // var containingProject = $(this).closest(".project");
+    // containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+  	   $(".project-description").fadeToggle()
+       // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+}
+
+
+/*
+ * Function that is called when the document is ready.
+ */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Hi, how are you?");
+		$(".jumbotron p").toggleClass("active");
+		$("#testjs").text("Please wait...");
 	});
+
+    $("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
