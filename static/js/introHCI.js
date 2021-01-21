ac33bbba2e7597cc14e54824e0f30c753eb5c543
Ali Buzayan <abuzayan@ucsd.edu>
2021-01-21 10:36:30 -0800

    removed useless comments

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,17 +5,6 @@ $(document).ready(function() {
 	initializePage();
 })
 
-/*
- * Function that is called when the document is ready.
- */
-// function initializePage() {
-// 	$("#testjs").click(function(e) {
-// 		$('.jumbotron h1').text("Javascript is connected");
-// 	});
-
-// 	// Add any additional listeners here
-// 	// example: $("#div-id").click(functionToCall);
-// }
 function projectClick(e) {
     e.preventDefault();
     $(this).css("background-color", "#7fff00");
